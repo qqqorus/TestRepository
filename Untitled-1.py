da7d95e99da6b2e94738fef82c7db35529937227
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2024-10-02 14:40:45 +0400

    rawr
    testing only

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,21 @@
-print("Hello, World")
+#Test Repository
+#print("Hello, World")
+
+#assigning variables
+#a, b, c = "ozone", "layer", "yah"
+#print(a, b, c)
+
+#repeating names???
+#name = "Shuakat" * 7
+#print(name)
+
+#range(start, stop, step)
+#for i in range(12, 0, -2):
+#    print(i, end = ' ')
+
+#subtracting???
+#name = "Nick Jonas"
+#print(name[9::-1])
+
+group = input("What kpop group do u stan?: ")
+print("Wow, you stan", group,"?")
