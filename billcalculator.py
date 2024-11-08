2af14e678cba93f69571fa5bee77b467ed8ad2cc
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2024-11-08 10:35:25 +0400

    Delete billcalculator.py

--- billcalculator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the bill calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? (10 12 15) "))
-people = int(input("How many people will split the bill? "))
-payment = round((bill * (tip / 100) + bill) / people, 2)
-
-print(f"Each person should pay: {payment}")
