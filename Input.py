4b6720afaee086f66d02bd08d1d6278376501f51
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2024-11-08 10:40:56 +0400

    input notes

---
+++ Input.py
@@ -0,0 +1,7 @@
+# inputs!
+# variable = input(prompt)
+    # prompt is typically a string instructing user to enter a value
+
+# this is an example of how to execute the input function yay!
+user_name = input("What is your name? ")
+print(f"Hi, {user_name}!")
