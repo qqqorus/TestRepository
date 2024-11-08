11dc79e3b62c0fec7d950125ce24e532bda4c9b0
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2024-11-08 10:41:33 +0400

    print notes

---
+++ Print.py
@@ -0,0 +1,85 @@
+# Print
+
+print("Hello, world!")
+print("Quishia Desiree Famador")
+print(5 + 7)
+
+# this is a string from top 2 bawt-um
+print('Programming')
+print('is')
+print('fun!')
+
+# this is a comment
+# rararaaa rumarumama gaga oo lala want ur bad romance
+# input 
+name = input("What is your name? ")
+number = int(input("Enter a number: "))
+score = float(input("Enter your score: "))
+
+# '=' assignment operator
+# '==' comparison operator
+# truncated - removes the decimals
+# rounded off - rounds the number to the nearest number if it's greater than 5
+
+a = 27
+b = 3
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+quotient_int = a // b
+remainder = a % b
+power = a ** b
+
+# addition
+print(f"The sum of {a} and {b} is, {sum}.")
+
+# subtraction
+print(f"The difference of {a} and {b} is, {difference}.")
+
+# multiplication
+print(f"The product of {a} and {b} is, {product}.")
+
+# division
+print(f"The quotient of {a} and {b} is, {quotient}.")
+
+# integer division
+print(f"The floored quotient of {a} and {b} is, {quotient_int}.")
+
+# remainder
+print(f"The remainder of {a} and {b} is, {remainder}.")
+
+# exponent
+print(f"The power of {a} and {b} is, {power}.")
+print(3 * 0.1)
+
+# '\' multiline continuation, for without parentheses
+# '\n' new line
+# '\t' new tab
+
+monday = 12445
+tuesday = 23456
+wednesday = 78921
+
+print("Monday's sales are", monday,
+      "and Tuesday's sales are", tuesday,
+      "and Wednesday's sales are", wednesday)
+
+# string concatenation - joining smth
+message = 'Hello ' + 'world'
+print(message)
+
+print('Enter the amount of ' +
+      'sales for each day and ' + 
+      'press Enter.')
+
+name = "Yoji"
+print(f'Hello {name}.')
+print(f'The value is {10 + 2}.')
+
+val = 10
+print(f'The value is {val + 2}.')
+
+num = 123.456789
+print(f'{num:.2f}')
+# '.2f' rounds the value to 2 decimal places, displays the value as a floating-point number
